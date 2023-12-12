239edad228673b547f4cc88e92544c515bc652ae
Zhengxuan Wen <wenzhengxuan@gmail.com>
2023-12-12 03:36:41 -0500

    /

--- app.py
+++ app.py
@@ -402,4 +402,4 @@ def index():
     return render_template('index.html', active_section='busiestStations')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port=8080)
